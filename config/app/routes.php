11a8a2cde532b3fac4a4eab29f37eb5cea726f49
Alexey Shmelev <alexsey_89@bk.ru>
2021-06-14 07:48:05 +0300

    added diff changes

--- config/app/routes.php
+++ config/app/routes.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 use Slim\App;
 
 use Solutions\Ci\Http\Controllers;
-use Tuupola\Middleware\HttpBasicAuthentication;
 
 return static function (App $app): void {
     $app->get('/', Controllers\IndexController\HomeAction::class)->setName('home');
     $app->map(['GET', 'POST'],'/category/create', Controllers\CategoryController\CreateAction::class);
-    $app->get('/category/list', Controllers\CategoryController\ListAction::class);
+    $app->get('/category/view/{id}', Controllers\CategoryController\ViewAction::class.':view');
+    $app->map(['GET', 'POST'],'/category/update/{id}', Controllers\CategoryController\UpdateAction::class.':update');
 };
--- config/main/view.php
+++ config/main/view.php
@@ -2,14 +2,24 @@
 
 use Psr\Container\ContainerInterface;
 use Slim\Views\PhpRenderer;
+use Solutions\Modules\Solutions\Query\GetCategories\GetCategories;
+use Solutions\Support\BreadcrumbsTree;
+use Solutions\Support\HtmlTree;
 
 return [
-    PhpRenderer::class => static function (ContainerInterface $container) {
+    PhpRenderer::class => static function (ContainerInterface $container, GetCategories $getCategories) {
         $view = new PhpRenderer($container->get('configurations')['view']['TemplatePath']);
         $view->setLayout($container->get('configurations')['view']['Layout']);
         $view->addAttribute('css', ['bootstrap.min.css', 'style.css']);
         $view->addAttribute('js', ['jquery-3.6.0.slim.min.js', 'bootstrap.bundle.min.js', 'scripts.js']);
 
+        $treeCategories = HtmlTree::create($getCategories->handle(), ['parent' => 'parent_id'],[],false);
+
+        $view->addAttribute('categories', $treeCategories->showTree());
+
+        //$breadcrumbs = BreadcrumbsTree::create($getCategories->handle(), ['parent' => 'parent_id'],[],false);
+
+        //print_r($breadcrumbs->getParents(['parent_id' => 'a01e5501-df80-4ab8-a85c-23430dd57622']));
         return $view;
     },
 
